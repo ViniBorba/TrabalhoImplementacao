be99401e308b0f895ed700e7dfd1fcdc5e755054
Vinicius Borba <vinichius@gmail.com>
2017-10-31 17:42:06 -0200

    criei as classes principais, e o esqueleto do sistema

---
+++ src/Evento.java
@@ -0,0 +1,14 @@
+import Palestra.Palestra;
+
+
+public class Evento {
+	
+	private int data;//Tem que vim do arquivo csv
+	
+	Palestra palestra;//talvez um arrayList, ou , pq cada palestra vai ser uma lista, um arquivo csv
+	
+	Participantes participantes;//Participantes por palestra? Ou do evento todo? Se for do evento vai ter que ter um arrayList Tambem
+	
+	Certificado certificado;//Certificado vai ser uma classe asbtrata
+
+}
---
+++ src/br/com/vini/tranalhoimplementacao/palestra/Palestra.java
@@ -0,0 +1,12 @@
+package br.com.vini.tranalhoimplementacao.palestra;
+
+public class Palestra {
+	
+	//dados tem que vir do arquivo lido
+	private String Sala;
+	private int hora;
+	private String titulo;
+	private String resumoDaPalestra;
+	
+	Palestrante palestrante;
+}
