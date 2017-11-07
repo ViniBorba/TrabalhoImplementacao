f95b02a88ae0d6ddefddcff578fbe792fab17b3c
Vinicius Borba <vinichius@gmail.com>
2017-11-07 11:17:24 -0200

    separador de virgulas colocado no construtor da class Evento

--- src/br/com/vini/tranalhoimplementacao/palestra/Palestra.java
+++ src/br/com/vini/tranalhoimplementacao/palestra/Palestra.java
@@ -1,19 +1,35 @@
 package br.com.vini.tranalhoimplementacao.palestra;
 
+import java.util.Scanner;
+
 import br.com.vini.tranalhoimplementacao.arquivoscsv.LeitorDeArquivos;
 
 
 public class Palestra {
 	
 	//dados tem que vir do arquivo lido
-	String sala;
+	public String sala = null;
 	private String hora;
 	private String titulo;
 	String resumoDaPalestra;
 	
 	private Palestrante palestra;
 	
-	public Palestra(String sala, String hora, String titulo, String resumoDaPalestra ){
+	//COnstrutor de Palestra
+	public Palestra(Scanner arquivoComDados){
+		
+		//arquivoComDados.useDelimiter("[,\n]");
+		
+		//this.sala = arquivoComDados.next();
+		
+		while(arquivoComDados.hasNext()){
+			arquivoComDados.useDelimiter("[,\n]");
+			
+			if(this.sala == null)
+			
+			this.sala = arquivoComDados.next();
+		}
+		
 		sala = this.sala;
 		hora = this.hora;
 		titulo = this.titulo;
@@ -26,13 +42,22 @@ public class Palestra {
 		return sala;
 	}
 	
+	
+	public String getSala() {
+		return sala;
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args){
 		
 		LeitorDeArquivos carregaPalestra = new LeitorDeArquivos("/home/vini/Imagens/palestras.csv");
 		
-		Palestra palestra1 = new Palestra(carregaPalestra.getArquivo().next(), carregaPalestra.getArquivo().next(), carregaPalestra.getArquivo().next(), carregaPalestra.getArquivo().next());
+		Palestra palestra1 = new Palestra(carregaPalestra.getArquivo());
 		
-		System.out.println(palestra1);
+		System.out.println(palestra1.getSala());
 		
 		/*
 		while(carregaPalestra.facaAte()){
